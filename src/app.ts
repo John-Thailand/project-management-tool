dc18d5acb7b0bb8cfa994f3f517019fe56bef2a2
John-Thailand <naoki.honda@mirrorfit.jp>
2024-04-24 16:34:57 +0900

    DOM要素の操作

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,9 @@ class ProjectInput {
     templateElement: HTMLTemplateElement;
     hostElement: HTMLDivElement;
     element: HTMLFormElement;
+    titleInputElement: HTMLInputElement;
+    descriptionInputElement: HTMLInputElement;
+    mandayInputElement: HTMLInputElement;
 
     constructor() {
         this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement;
@@ -9,9 +12,34 @@ class ProjectInput {
 
         const importedNode = document.importNode(this.templateElement.content, true);
         this.element = importedNode.firstElementChild as HTMLFormElement;
+        this.element.id = 'user-input';
+
+        this.titleInputElement = this.element.querySelector(
+            '#title',
+        ) as HTMLInputElement;
+        this.descriptionInputElement = this.element.querySelector(
+            '#description',
+        ) as HTMLInputElement;
+        this.mandayInputElement = this.element.querySelector(
+            '#manday',
+        ) as HTMLInputElement;
+
+        this.configure();
         this.attach();
     }
 
+    private submitHandler(event: Event) {
+        // submitイベントの本来の動作を止める
+        event.preventDefault();
+        console.log(this.titleInputElement.value);
+    }
+
+    // eventListenerの追加
+    private configure() {
+        this.element.addEventListener('submit', this.submitHandler.bind(this));
+    }
+
+    // 要素を追加
     private attach() {
         this.hostElement.insertAdjacentElement('afterbegin', this.element);
     }
