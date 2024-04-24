2760e02dfca21be186b8560415b02da6be2fc216
John-Thailand <naoki.honda@mirrorfit.jp>
2024-04-24 17:13:28 +0900

    ユーザー入力の取得

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,21 @@
+// qutobind decorator
+function autobind(
+    _: any,
+    __: string,
+    descriptor: PropertyDescriptor,
+) {
+    const originalMethod = descriptor.value;
+    const adjDescriptor: PropertyDescriptor = {
+        configurable: true,
+        get() {
+            const boundFn = originalMethod.bind(this);
+            return boundFn;
+        }
+    }
+    return adjDescriptor;
+}
+
+// ProjectInput Class
 class ProjectInput {
     templateElement: HTMLTemplateElement;
     hostElement: HTMLDivElement;
@@ -28,15 +46,45 @@ class ProjectInput {
         this.attach();
     }
 
+    private gatherUserInput(): [string, string, number] | void {
+        const enteredTitle = this.titleInputElement.value;
+        const enteredDescription = this.descriptionInputElement.value;
+        const enteredManday = this.mandayInputElement.value;
+        if (
+            enteredTitle.trim().length === 0 ||
+            enteredDescription.trim().length === 0 ||
+            enteredManday.trim().length === 0
+        ) {
+            alert('入力値が正しくありません。再度お試しください。');
+            return;
+        } else {
+            // +を前につけることでstring型からnumber型へ変換できる
+            return [enteredTitle, enteredDescription, +enteredManday];
+        }
+    }
+
+    private clearInputs() {
+        this.titleInputElement.value = '';
+        this.descriptionInputElement.value = '';
+        this.mandayInputElement.value = '';
+    }
+
+    @autobind
     private submitHandler(event: Event) {
         // submitイベントの本来の動作を止める
         event.preventDefault();
-        console.log(this.titleInputElement.value);
+        const userInput = this.gatherUserInput();
+        // TuppleはArrayなのでArrayかどうか判定する
+        if (Array.isArray(userInput)) {
+            const [title, desc, manday] = userInput;
+            console.log(title, desc, manday);
+            this.clearInputs();
+        }
     }
 
     // eventListenerの追加
     private configure() {
-        this.element.addEventListener('submit', this.submitHandler.bind(this));
+        this.element.addEventListener('submit', this.submitHandler);
     }
 
     // 要素を追加
