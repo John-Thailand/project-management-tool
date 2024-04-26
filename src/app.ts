7111081bada723f76e85f08c796aff0545a8d0de
John-Thailand <naoki.honda@mirrorfit.jp>
2024-04-26 13:29:50 +0900

    Enumによるプロジェクトのフィルタリング

--- src/app.ts
+++ src/app.ts
@@ -131,7 +131,13 @@ class ProjectList {
         this.element.id = `${this.type}-projects`;
 
         projectState.addListener((projects: Project[]) => {
-            this.assignedProjects = projects;
+            const relevantProjects = projects.filter(prj => {
+                if (this.type === 'active') {
+                    return prj.status === ProjectStatus.Active;
+                }
+                return prj.status === ProjectStatus.Finished;
+            });
+            this.assignedProjects = relevantProjects;
             this.renderProjects();
         });
 
@@ -141,6 +147,8 @@ class ProjectList {
 
     private renderProjects() {
         const listEl = document.getElementById(`${this.type}-projects-list`)! as HTMLUListElement;
+        // プロジェクトリストをクリアする
+        listEl.innerHTML = '';
         for (const prjItem of this.assignedProjects) {
             const listItem = document.createElement('li');
             listItem.textContent = prjItem.title;
