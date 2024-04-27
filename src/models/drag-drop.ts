ceeaf82febb787774fb3a98279bad30303b235cb
John-Thailand <naoki.honda@mirrorfit.jp>
2024-04-27 17:54:03 +0900

    ESモジュールの利用

--- src/models/drag-drop.ts
+++ src/models/drag-drop.ts
@@ -1,14 +1,12 @@
-namespace App {
-    // Drag & Drop
-    // export: 他のファイルから参照できる
-    export interface Draggable {
-        dragStartHandler(event: DragEvent): void;
-        dragEndHandler(event: DragEvent): void;
-    }
+// Drag & Drop
+// export: 他のファイルから参照できる
+export interface Draggable {
+    dragStartHandler(event: DragEvent): void;
+    dragEndHandler(event: DragEvent): void;
+}
 
-    export interface DragTarget {
-        dragOverHandler(event: DragEvent): void;
-        dropHandler(event: DragEvent): void;
-        dragLeaveHandler(event: DragEvent): void;
-    }
+export interface DragTarget {
+    dragOverHandler(event: DragEvent): void;
+    dropHandler(event: DragEvent): void;
+    dragLeaveHandler(event: DragEvent): void;
 }
--- src/util/validation.ts
+++ src/util/validation.ts
@@ -1,46 +1,44 @@
-namespace App {
-    //Validation
-    export interface Validatable {
-        value: string | number;
-        required?: boolean;
-        minLength?: number;
-        maxLength?: number;
-        min?: number;
-        max?: number;
-    }
+//Validation
+export interface Validatable {
+    value: string | number;
+    required?: boolean;
+    minLength?: number;
+    maxLength?: number;
+    min?: number;
+    max?: number;
+}
 
-    export function validate(validatableInput: Validatable) {
-        let isValid = true;
-        // 空チェック
-        if (validatableInput.required) {
-            isValid = isValid && validatableInput.value.toString().trim().length !== 0;
-        }
-        // 最小文字数以上であるか
-        if (validatableInput.minLength != null &&
-            typeof validatableInput.value === 'string'
-        ) {
-            isValid = isValid && validatableInput.value.length >= validatableInput.minLength;
-        }
-        // 最大文字数以下であるか
-        if (validatableInput.maxLength != null &&
-            typeof validatableInput.value === 'string'
-        ) {
-            isValid = isValid && validatableInput.value.length <= validatableInput.maxLength;
-        }
-        // 最小値以上であるか
-        if (
-            validatableInput.min != null &&
-            typeof validatableInput.value === 'number'
-        ) {
-            isValid = isValid && validatableInput.value >= validatableInput.min;
-        }
-        // 最大値以上であるか
-        if (
-            validatableInput.max != null &&
-            typeof validatableInput.value === 'number'
-        ) {
-            isValid = isValid && validatableInput.value <= validatableInput.max;
-        }
-        return isValid;
+export function validate(validatableInput: Validatable) {
+    let isValid = true;
+    // 空チェック
+    if (validatableInput.required) {
+        isValid = isValid && validatableInput.value.toString().trim().length !== 0;
     }
+    // 最小文字数以上であるか
+    if (validatableInput.minLength != null &&
+        typeof validatableInput.value === 'string'
+    ) {
+        isValid = isValid && validatableInput.value.length >= validatableInput.minLength;
+    }
+    // 最大文字数以下であるか
+    if (validatableInput.maxLength != null &&
+        typeof validatableInput.value === 'string'
+    ) {
+        isValid = isValid && validatableInput.value.length <= validatableInput.maxLength;
+    }
+    // 最小値以上であるか
+    if (
+        validatableInput.min != null &&
+        typeof validatableInput.value === 'number'
+    ) {
+        isValid = isValid && validatableInput.value >= validatableInput.min;
+    }
+    // 最大値以上であるか
+    if (
+        validatableInput.max != null &&
+        typeof validatableInput.value === 'number'
+    ) {
+        isValid = isValid && validatableInput.value <= validatableInput.max;
+    }
+    return isValid;
 }
