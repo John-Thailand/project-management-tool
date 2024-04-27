84436be804c6a51acbf18d732e6623a361643b40
John-Thailand <naoki.honda@mirrorfit.jp>
2024-04-27 20:54:46 +0900

    ts-loaderの利用

--- webpack.config.js
+++ webpack.config.js
@@ -7,5 +7,20 @@ module.exports = {
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
+    },
+    devtool: 'inline-source-map',
+    // tsファイルをts-loaderでコンパイルする
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: 'ts-loader',
+                exclude: /mode_modules/
+            }
+        ]
+    },
+    // インポートされたモジュールをどう解決するか
+    resolve: {
+        extensions: ['.ts', '.js']
     }
 };
